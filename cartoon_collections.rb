b33c14a8ced1e2d1dbd7a6c076999e337fcdbc3e
Yurina Shin <yurinas@gmail.com>
2017-12-10 17:54:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,8 @@ end
 def find_the_cheese(array)
 
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.map {|item| item.include?(cheese_types)}
-    
-    return item
+  if array.each {|item| item.include?(cheese_types)}
+puts item
   # array.map.include?(cheese_types)
 
   end
