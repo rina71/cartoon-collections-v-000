8ecc79d16461dfbcd567764902c9dd35a222dffd
Yurina Shin <yurinas@gmail.com>
2017-12-10 17:29:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index{|name, i|puts "#{i + 1}.*#{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+array
+array.collect{|name| name.capitalize << "!"}
+
 end
 
 def long_planeteer_calls# code an argument here
